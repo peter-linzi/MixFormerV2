4f55f1dbad7f6b1b335398378ef071b3426a47a3
linfanyu <linfanyu@aeronitor.com>
2023-11-15 10:14:54 +0800

    chore: add test script of uav

--- tracking/test_mixformer.sh
+++ tracking/test_mixformer.sh
@@ -1,11 +1,19 @@
 ##########-------------- MixFormerV2-Base-----------------##########
 
-### LaSOT test and evaluation
+### UAV test and evaluation
 python tracking/test.py mixformer2_vit_online 288_depth8_score \
- --dataset lasot --threads 32 --num_gpus 8 \
+ --dataset uav --threads 2 --num_gpus 1 \
  --params__model models/mixformerv2_base.pth.tar \
  --params__search_area_scale 5.0 \
+ --params__online_size 1 \
  --debug 0
+
+# ### LaSOT test and evaluation
+# python tracking/test.py mixformer2_vit_online 288_depth8_score \
+#  --dataset lasot --threads 32 --num_gpus 8 \
+#  --params__model models/mixformerv2_base.pth.tar \
+#  --params__search_area_scale 5.0 \
+#  --debug 0
 # python tracking/analysis_results.py --dataset_name lasot --tracker_param 288_depth8_score
 
 ### TrackingNet test and pack
